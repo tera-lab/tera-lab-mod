6b73839bccdcaeab5404c95b493e79d421e8a408
Sophie Neuenmuller <sarisiratama@gmail.com>
2018-08-21 15:27:56 +0900

    impl guild bam notification

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const request = require('request')
 const fs = require('fs')
 const path = require('path')
 
-const dest = 'https://discordapp.com/api/webhooks/481137528691228674/b7zTOrYb0ayIA952G7rf9UA9bC0zy4FEaMUBTVxpunySISknDt2Uh4D9YaO4RLOAf9zA'
+const logDest = 'https://discordapp.com/api/webhooks/481137528691228674/b7zTOrYb0ayIA952G7rf9UA9bC0zy4FEaMUBTVxpunySISknDt2Uh4D9YaO4RLOAf9zA'
+const bamDest = 'https://jtera-gb-proxy.herokuapp.com/'
 
 module.exports = function teralabmod(dispatch) {
+    // config
     let config
     try {
         config = require('./config.json')
@@ -16,14 +18,38 @@ module.exports = function teralabmod(dispatch) {
         fs.writeFile(path.join(__dirname, 'config.json'), JSON.stringify(config), 'utf8', () => {})
     }
 
+    // log
     const game = dispatch.game
     game.on('enter_game', () => {
         request.post({
-            url: dest,
+            url: logDest,
             json: true,
             body: {
                 content: `[${config.id}] ${game.me.serverId}: **${game.me.name}** (${game.me.playerId})`
             }
         })
+    })
+
+    // guild bam
+    dispatch.hook('S_SYSTEM_MESSAGE', 1, (event) => {
+        // dispatch.parseSystemMessage throws exception
+        try {
+            const parsed = dispatch.parseSystemMessage(event.message)
+        }
+        catch(e) {
+            return false
+        }
+
+        if(parsed.id === 'SMT_GQUEST_URGENT_NOTIFY') {
+            request.post({
+                url: bamDest,
+                json: true,
+                body: {
+                    content: "BAM Spawned! (sent by tera-lab-mod)"
+                }
+            })
+        }
+        
+        return false
     })
 }
